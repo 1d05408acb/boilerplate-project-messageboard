2c2aaf137f3cc7c76e88ff2bd12f83718e508a28
Bjørnar Borge <72221999+1d05408acb@users.noreply.github.com>
2022-10-27 14:13:13 +0200

    Update db-connection.js

--- db-connection.js
+++ db-connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = mongoose.connect(process.env.DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 });
 
 module.exports = db;
